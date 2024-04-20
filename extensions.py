a35005e91b6e2b694ed1b366546d0224a8e3f99a
SergejBogdanov <155895144+SergejBogdanov@users.noreply.github.com>
2024-04-20 21:00:57 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,1 +1,38 @@
+import json
+import requests
+from config import *
 
+
+class APIException(Exception):
+    pass
+
+
+class Exchange:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не смог обработать валюту "{quote}".')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не смог обработать валюту "{base}".')
+
+        if quote == base:
+            raise APIException(
+                f'Нельзя перевести одинаковые валюты "{base}".')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не смог обработать количество "{amount}".')
+
+        r = requests.get(
+            f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+
+        total_base = round((float(json.loads(r.content)[keys[base]]) * amount), 2)
+
+        return total_base
